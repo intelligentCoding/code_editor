7aec6d1fc9029738ad49dcfa69fb8f0cd7e36947
kashif mahmood <kaaf_kaaf@hotmail.com>
2022-03-31 09:22:45 -0600

    debouncing logic for code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Resizable from './resizable';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -7,11 +7,15 @@ const CodeCell = () => {
     const [ code, setCode ] = useState('')
     const [input, setInput] = useState('');
 
-    const onClick = async () => {
+    useEffect(() => {
+      const timer = setTimeout(async () => {
         const output = await bundle(input);
         setCode(output);
-    }
-
+      }, 1000)
+      return () => {
+        clearTimeout(timer);
+      }
+    }, [input])
 
     return (
       <Resizable direction="vertical">   
