70f0d40fdb053856f298eaeabd10168c7ca157a7
kashif mahmood <kaaf_kaaf@hotmail.com>
2022-04-16 13:30:35 -0600

    initialized action bar

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,11 +1,12 @@
 import { Cell } from '../state';
 import CodeCell from './code-cell';
 import TextEditor from './text-editor';
-
+import ActionBar from './action-bar';
 
 interface CellListItemProps {
   cell: Cell
 }
+
 const CellListItem: React.FC<CellListItemProps> = ({cell}) => {
   let child: JSX.Element;
   if(cell.type === 'code') {
@@ -13,7 +14,10 @@ const CellListItem: React.FC<CellListItemProps> = ({cell}) => {
   } else {
     child = <TextEditor cell={cell}/>
   }
-  return <div>{child}</div>
+  return <div>
+    <ActionBar id={cell.id}/> 
+    {child}
+    </div>
 }
 
 export default CellListItem;
